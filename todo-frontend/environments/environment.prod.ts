89ed5e8723f01bc7e5265daa3724f51021d99628
vortep <python.vortep@gmail.com>
2024-10-22 20:03:46 +0300

    correct structure project

--- todo-frontend/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  getEntriesUrl: 'http://77.105.140.209:8000/api/entries/',
-  updateEntriesUrl: 'http://77.105.140.209:8000/api/save_entries/'
-};
-
