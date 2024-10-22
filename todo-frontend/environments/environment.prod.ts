aea4add17e86ebc756d1205410ed4b5cb8744410
vortep <python.vortep@gmail.com>
2024-10-22 19:24:25 +0300

    commit project

---
+++ todo-frontend/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  getEntriesUrl: 'http://77.105.140.209:8000/api/entries/',
+  updateEntriesUrl: 'http://77.105.140.209:8000/api/save_entries/'
+};
+
